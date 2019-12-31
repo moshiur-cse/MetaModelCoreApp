c98cc79af3a020a3e00ea5a3272da3d45e0b21e9
Moshiur Rahman <moshiur_cse@hotmail.com>
2019-12-31 16:42:12 +0600

    Update Map

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -62,6 +62,11 @@ namespace MetaModelCoreApp.Controllers
             return View();
         }
 
+        public IActionResult MapView()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
